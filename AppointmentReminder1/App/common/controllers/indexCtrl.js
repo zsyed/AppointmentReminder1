3d2dac879b35e7af08719a0e323d21e083c2e0d8
zsyed <zulfiqarsyed@yahoo.com>
2015-02-25 21:01:51 -0800

    add indexCtrl file.

---
+++ AppointmentReminder1/App/common/controllers/indexCtrl.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    angular
+        .module("appointmentReminderModule")
+        .controller("indexCtrl", ["$scope","profileService", indexCtrl]);
+
+    function indexCtrl($scope,profileService) {
+
+        $scope.profileExists = function () {
+            return (profileService.getProfile() != null);
+        }
+
+    }
+}());
